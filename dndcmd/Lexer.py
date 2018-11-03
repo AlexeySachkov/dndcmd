9bd5440ca8a400462a9f4182bed43061bd1ee467
Alexey Sachkov <sachkov2011@gmail.com>
2018-11-03 13:04:25 +0300

    Add simple lexer for future /roll command

---
+++ dndcmd/Lexer.py
@@ -0,0 +1,68 @@
+import enum
+
+
+class Token(enum.Enum):
+    TOK_UNKNOWN = -1
+    TOK_EOL = 0
+    TOK_NUMBER = 1
+    TOK_DICE = 2
+    TOK_PAREN = 3
+    TOK_OP = 4
+
+
+class Lexer:
+    def __init__(self):
+        self.str = ''
+        self.pos = 0
+        self.last_char = ''
+        self.cur_token = Token.TOK_UNKNOWN
+
+    def tokenize(self, input_str):
+        self.str = input_str
+        self.pos = 0
+        self.last_char = ' '
+
+        tokens = []
+        while True:
+            tok = self.get_next_token()
+            tokens.append(tok)
+            if tok[0] == Token.TOK_EOL:
+                break
+
+        return tokens
+
+    def get_next_token(self):
+        self.cur_token = self.get_token()
+        return self.cur_token
+
+    def get_token(self):
+        while self.pos < len(self.str) and self.str[self.pos].isspace():
+            self.pos = self.pos + 1
+
+        if self.pos >= len(self.str):
+            return Token.TOK_EOL,
+
+        if self.str[self.pos].isdigit():
+            num_str = self.str[self.pos]
+            self.pos = self.pos + 1
+            while self.pos < len(self.str) and self.str[self.pos].isdigit():
+                num_str += self.str[self.pos]
+
+            return Token.TOK_NUMBER, int(num_str)
+
+        if self.str[self.pos] == 'd':
+            self.pos = self.pos + 1
+            return Token.TOK_DICE,
+
+        if self.str[self.pos] == '(' or self.str[self.pos] == ')':
+            paren = self.str[self.pos]
+            self.pos = self.pos + 1
+            return Token.TOK_PAREN, paren
+
+        if self.str[self.pos] == '+' or self.str[self.pos] == '*' or \
+                self.str[self.pos] == '-' or self.str[self.pos] == '/':
+            op = self.str[self.pos]
+            self.pos = self.pos + 1
+            return Token.TOK_OP, op
+
+        return Token.TOK_UNKNOWN, self.str[self.pos]
---
+++ tests/test_lexer.py
@@ -0,0 +1,42 @@
+import unittest
+from dndcmd.Lexer import Lexer, Token
+
+
+class LexerTest(unittest.TestCase):
+    def setUp(self):
+        self.lex = Lexer()
+
+    def helper(self, input, reference):
+        tokens = self.lex.tokenize(input)
+        self.assertEqual(reference, tokens)
+
+    def test_eol(self):
+        self.helper('', [(Token.TOK_EOL,)])
+
+    def test_eol_with_spaces(self):
+        self.helper('     ', [(Token.TOK_EOL,)])
+
+    def test_number(self):
+        self.helper('234', [(Token.TOK_NUMBER, 234), (Token.TOK_EOL,)])
+
+    def test_dice(self):
+        self.helper('d', [(Token.TOK_DICE,), (Token.TOK_EOL,)])
+
+    def test_parens(self):
+        self.helper('()', [(Token.TOK_PAREN, '('), (Token.TOK_PAREN, ')'),
+                           (Token.TOK_EOL,)])
+
+    def test_op(self):
+        self.helper('+*-/', [(Token.TOK_OP, '+'), (Token.TOK_OP, '*'),
+                             (Token.TOK_OP, '-'), (Token.TOK_OP, '/'),
+                             (Token.TOK_EOL,)])
+
+    def test_simple(self):
+        self.helper('2d6 + 4', [(Token.TOK_NUMBER, 2), (Token.TOK_DICE,),
+                                (Token.TOK_NUMBER, 6), (Token.TOK_OP, '+'),
+                                (Token.TOK_NUMBER, 4), (Token.TOK_EOL,)])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
